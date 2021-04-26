e1ebce392704faef6521239426b82dd2e7821a53
Albert González <albert@mailtrack.io>
2021-04-26 23:21:05 +0200

    init game moved to file + style fixes

---
+++ src/game/init.ts
@@ -0,0 +1,42 @@
+import "phaser";
+
+import "regenerator-runtime/runtime";
+import { init } from "./i18n/i18n";
+import { titleSceneConfig } from "./scenes/title/titleSceneConfig";
+import { scene1Config, SCENE_1_KEY } from "./scenes/scene1/config";
+import { scene2Config, SCENE_2_KEY } from "./scenes/scene2/config";
+import { scene3Config, SCENE_3_KEY } from "./scenes/scene3/config";
+import { isSoundMuted } from "./scenes/common/audio";
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+  },
+  scale: {
+    parent: "game",
+    max: { width: 1024, height: 768 },
+    expandParent: false,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    fullscreenTarget: "game",
+  },
+  input: {
+    activePointers: 2,
+  },
+};
+
+export default (): void => {
+  init();
+  const game = new Phaser.Game({
+    ...gameConfig,
+    scene: titleSceneConfig,
+  });
+
+  game.sound.mute = isSoundMuted();
+  game.scene.add(SCENE_1_KEY, scene1Config);
+  game.scene.add(SCENE_2_KEY, scene2Config);
+  game.scene.add(SCENE_3_KEY, scene3Config);
+};
--- src/init.ts
+++ src/init.ts
@@ -1,43 +1,8 @@
 import "phaser";
 
 import "regenerator-runtime/runtime";
-import { init } from "./game/i18n/i18n";
-import { titleSceneConfig } from "./game/scenes/title/titleSceneConfig";
-import { scene1Config, SCENE_1_KEY } from "./game/scenes/scene1/config";
-import { scene2Config, SCENE_2_KEY } from "./game/scenes/scene2/config";
-import { scene3Config, SCENE_3_KEY } from "./game/scenes/scene3/config";
-import { isSoundMuted } from "./game/scenes/common/audio";
 import initDropdown from "./web/ts/dropdown";
+import initGame from "./game/init";
 
-const gameConfig: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-  },
-  scale: {
-    parent: "game",
-    max: { width: 1024, height: 768 },
-    expandParent: false,
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    fullscreenTarget: "game",
-  },
-  input: {
-    activePointers: 2,
-  },
-};
-
-init();
+initGame();
 initDropdown();
-
-const game = new Phaser.Game({
-  ...gameConfig,
-  scene: titleSceneConfig,
-});
-
-game.sound.mute = isSoundMuted();
-game.scene.add(SCENE_1_KEY, scene1Config);
-game.scene.add(SCENE_2_KEY, scene2Config);
-game.scene.add(SCENE_3_KEY, scene3Config);
